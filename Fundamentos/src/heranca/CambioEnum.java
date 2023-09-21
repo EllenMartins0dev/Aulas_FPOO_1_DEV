326bf01ae4f2ae6c30e44bbf10247dcfc87b760c
Ellen <ellenmartins@aluno.senai.br>
2023-09-21 16:40:47 -0300

    Enviando a atv de Herança

--- Fundamentos/src/heranca/CambioEnum.java
+++ Fundamentos/src/heranca/CambioEnum.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package heranca;
 
-/**
- *
- * @author Aluno
- */
 public enum CambioEnum {
     AUTOMATICO("Automático"),
     MANUAL("Manual");
