00d3ddc133005fb15d560b91910ff5685a0fddac
Ellen <ellenmartins@aluno.senai.br>
2023-11-23 15:27:12 -0300

    Acrescentando atividade sobre DataTime

---
+++ Fundamentos/src/datahora/DataHora.java
@@ -0,0 +1,14 @@
+
+package datahora;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DataHora {
+     public static void main(String[] args) throws UnsupportedEncodingException {
+        System.setOut(new PrintStream(System.out, true, "UTF-8"));
+        
+    }
+}
